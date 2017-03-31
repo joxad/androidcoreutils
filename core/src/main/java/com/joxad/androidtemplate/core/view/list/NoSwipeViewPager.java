bfe7357a397ac9f30c44e6906f242522ecb03076
Jocelyn David <external.z17jdavi>
2017-03-31 14:03:12 +0200

    Prepare bottom view navigation

---
+++ core/src/main/java/com/joxad/androidtemplate/core/view/list/NoSwipeViewPager.java
@@ -0,0 +1,33 @@
+package com.joxad.androidtemplate.core.view.list;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by josh on 26/08/16.
+ */
+public class NoSwipeViewPager extends ViewPager {
+
+
+    public NoSwipeViewPager(Context context) {
+        super(context);
+    }
+
+    public NoSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+}
