816f6f7ae19828df6bf44c02b4b82acc7e8c7bcb
Jocelyn David <external.z17jdavi>
2017-04-12 00:41:55 +0200

    Add internet check on start

---
+++ core/src/main/java/com/joxad/androidtemplate/core/ws/NetworkUtil.java
@@ -0,0 +1,13 @@
+package com.joxad.androidtemplate.core.ws;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+    public static boolean isOnline(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected());
+    }
+}
